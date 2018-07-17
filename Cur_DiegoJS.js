3f7f5be4c44cbfff2083f55c271afcd4e327a7ad
DiegoRinconC <diego84darc@gmail.com>
2018-07-17 07:03:20 -0500

    primer commit

--- Cur_DiegoJS.js
+++ Cur_DiegoJS.js
@@ -1,35 +1,13 @@
-// Dadas la division que contiene todas las pestañas y la de la pestaña que se 
-// quiere mostrar, la funcion oculta todas las pestañas a excepcion de esa.
-function cambiarPestanna(pestannas, pestanna) {
-
-    // Obtiene los elementos con los identificadores pasados.
-    pestanna = document.getElementById(pestanna.id);
-    listaPestannas = document.getElementById(pestannas.id);
-
-    // Obtiene las divisiones que tienen el contenido de las pestañas.
-    cpestanna = document.getElementById('c' + pestanna.id);
-    listacPestannas = document.getElementById('contenido' + pestannas.id);
-
-    i = 0;
-    // Recorre la lista ocultando todas las pestañas y restaurando el fondo 
-    // y el padding de las pestañas.
-    while (typeof listacPestannas.getElementsByTagName('div')[i] != 'undefined') {
-        $(document).ready(function () {
-            $(listacPestannas.getElementsByTagName('div')[i]).css('display', 'none');
-            $(listaPestannas.getElementsByTagName('li')[i]).css('background', '');
-            $(listaPestannas.getElementsByTagName('li')[i]).css('padding-bottom', '');
-        });
-        i += 1;
+    function mostrarPestana(n){
+        var pestanas = document.getElementsByClassName("pestana");
+        var cabecera = document.getElementsByClassName("cabecera");
+        for(i = 0; i<pestanas.length; i++){
+            if(pestanas[i].className.includes("p-activa")){
+                pestanas[i].className = pestanas[i].className.replace("p-activa", "");
+                cabecera[i].className = cabecera[i].className.replace("c-activa", "");
+                break;
+            }
+        }
+        pestanas[n].className += " p-activa";
+        cabecera[n].className += " c-activa";
     }
-
-    $(document).ready(function () {
-        // Muestra el contenido de la pestaña pasada como parametro a la funcion,
-        // cambia el color de la pestaña y aumenta el padding para que tape el  
-        // borde superior del contenido que esta juesto debajo y se vea de este 
-        // modo que esta seleccionada.
-        $(cpestanna).css('display', '');
-        $(pestanna).css('background', 'dimgray');
-        $(pestanna).css('padding-bottom', '2px');
-    });
-
-}
